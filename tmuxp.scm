bf333136c9cc31f18553dcc203d2d7f84de2aa92
SqrtMinusOne <thexcloud@gmail.com>
2021-06-19 20:56:37 +0300

    feat(tmuxp): add tmuxp

---
+++ tmuxp.scm
@@ -0,0 +1,76 @@
+(define-module (tmuxp)
+  #:use-module (guix git-download)
+  #:use-module (guix download)
+  #:use-module (guix build-system python)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages tmux)
+  #:use-module (gnu packages python-xyz))
+
+(define-public python-libtmux-0.10
+  (package
+   (name "python-libtmux")
+   (version "0.10.1")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "libtmux" version))
+     (sha256
+      (base32
+       "0qbbna7fkfvazhg3pr91rakkpj51cfa42kkh72aqkfhnjr4q3g68"))))
+   (build-system python-build-system)
+   (arguments
+    '(#:tests? #f))
+   (home-page
+    "https://github.com/tmux-python/libtmux")
+   (synopsis "scripting library / orm for tmux")
+   (description "scripting library / orm for tmux")
+   (license license:expat)))
+
+(define-public python-kaptan
+  (package
+   (name "python-kaptan")
+   (version "0.5.12")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "kaptan" version))
+     (sha256
+      (base32
+       "1lix7hafsqdpdmiyihykymb1x9bp2s02ik0smzjzq8hlfdb1zg8s"))))
+   (build-system python-build-system)
+   (propagated-inputs
+    `(("python-pyyaml" ,python-pyyaml)))
+   (arguments
+    '(#:tests? #f))
+   (home-page "https://github.com/emre/kaptan")
+   (synopsis "Configuration manager")
+   (description "Configuration manager")
+   (license license:bsd-3)))
+
+(define-public python-tmuxp
+  (package
+   (name "python-tmuxp")
+   (version "1.9.2")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "tmuxp" version))
+     (sha256
+      (base32
+       "0hy21qa80namd2s6zqhf1wkn7f7dpp59sbr32726nl5vi9n566fx"))))
+   (build-system python-build-system)
+   (arguments
+    '(#:tests? #f))
+   (propagated-inputs
+    `(("python-click" ,python-click)
+      ("python-colorama" ,python-colorama)
+      ("python-kaptan" ,python-kaptan)
+      ("python-libtmux" ,python-libtmux-0.10)))
+   (home-page
+    "https://github.com/tmux-python/tmuxp")
+   (synopsis "tmux session manager")
+   (description "tmux session manager")
+   (license license:expat)))
