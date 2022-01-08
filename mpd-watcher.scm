f727a64939691cb41a44d9d1b6546d76a44b233d
SqrtMinusOne <thexcloud@gmail.com>
2022-01-08 18:08:23 +0300

    feat(mpd-watcher): update

--- mpd-watcher.scm
+++ mpd-watcher.scm
@@ -3,12 +3,13 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system python)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages mpd))
 
 (define-public mpd-watcher
   (package
    (name "mpd-watcher")
-   (version "1.0.0")
+   (version "1.0.1")
    (source
     (origin
      (method git-fetch)
@@ -17,13 +18,14 @@
            (commit (string-append "v" version))))
      (file-name (git-file-name name version))
      (sha256
-      (base32 "0g5vxnyjaj95kiq6m04bq9qmn186xsjbzbzq0slghmnwc3mznb67"))))
+      (base32 "0vralrhzzld3b1mdw92ic0yw64nafkiks9qvjh9sgp2ff9f66lxp"))))
    (build-system python-build-system)
    (arguments
     '(#:tests? #f
       ))
    (native-inputs
-    `(("python-mpd2" ,python-mpd2)))
+    `(("python-mpd2" ,python-mpd2)
+      ("dynaconf" ,dynaconf)))
    (home-page "https://github.com/SqrtMinusOne/mpd-watcher/")
    (synopsis "A simple Python script to log mpd (https://www.musicpd.org/) activity in csv format.")
    (description "A simple Python script to log mpd (https://www.musicpd.org/) activity in csv format.")
