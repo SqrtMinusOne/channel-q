30751c0fe65b081e277c8270f82827c351665709
SqrtMinusOne <thexcloud@gmail.com>
2023-01-01 21:06:38 +0300

    feat(nushell): update

--- nushell-bin.scm
+++ nushell-bin.scm
@@ -15,16 +15,16 @@
 (define-public nushell-bin
   (package
     (name "nushell-bin")
-    (version "0.34.0")
+    (version "0.73.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append
              "https://github.com/nushell/nushell/releases/download/" version "/"
-             "nu_" (string-replace-substring version "." "_") "_linux.tar.gz"))
+             "nu-" version "-x86_64-unknown-linux-gnu.tar.gz"))
        (sha256
         (base32
-         "1qg560s0gaf38xmg3fpzx6lpcl6pnaz6bm07nn7zx49g5kh4jk7w"))))
+         "1k4jz4hcplvizpn38bww4za4vpffap1klndndinv3jx40yrdysgj"))))
     (build-system binary-build-system)
     (arguments
      `(#:phases
@@ -32,33 +32,16 @@
          (replace 'unpack
            (lambda* (#:key inputs #:allow-other-keys)
              (mkdir-p "nushell")
-             (invoke "tar" "xvzf" (assoc-ref inputs "source") "-C" "nushell" "--strip-components" "2"))))
+             (invoke "tar" "xvzf" (assoc-ref inputs "source") "-C" "nushell" "--strip-components" "1"))))
        #:install-plan
-       `(("nushell" "/bin/" #:exclude ("README.txt" "LICENSE" "libssl.so.1.1")))
+       `(("nushell" "/bin/" #:exclude ("README.txt" "LICENSE")))
        #:patchelf-plan
        `(("nushell/nu" ("glibc" "zlib" "gcc:lib"))
-         ("nushell/nu_plugin_binaryview" ("glibc" "gcc:lib" "libx11"))
-         ("nushell/nu_plugin_chart_bar" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_chart_line" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_fetch" ("glibc" "gcc:lib" "openssl" "zlib"))
-         ("nushell/nu_plugin_from_bson" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_from_mp4" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_from_sqlite" ("glibc" "gcc:lib"))
+         ("nushell/nu_plugin_custom_values" ("glibc" "gcc:lib"))
+         ("nushell/nu_plugin_example" ("glibc" "gcc:lib"))
+         ("nushell/nu_plugin_gstat" ("glibc" "gcc:lib" "zlib"))
          ("nushell/nu_plugin_inc" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_match" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_post" ("glibc" "gcc:lib" "openssl" "zlib"))
-         ("nushell/nu_plugin_ps" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_query_json" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_s3" ("glibc" "gcc:lib" "openssl"))
-         ("nushell/nu_plugin_selector" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_start" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_sys" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_textview" ("glibc" "gcc:lib" "zlib"))
-         ("nushell/nu_plugin_to_bson" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_to_sqlite" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_tree" ("glibc" "gcc:lib"))
-         ("nushell/nu_plugin_xpath" ("glibc" "gcc:lib")))
-       ))
+         ("nushell/nu_plugin_query" ("glibc" "gcc:lib")))))
     (inputs
      `(("curl" ,curl)
        ("gcc:lib" ,gcc "lib")
