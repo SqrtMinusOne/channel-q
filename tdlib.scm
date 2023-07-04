9ed64d6bc6f80836b17dca0ba150b437d193c6ef
SqrtMinusOne <thexcloud@gmail.com>
2023-07-04 12:50:00 +0300

    tdlib: update

--- tdlib.scm
+++ tdlib.scm
@@ -5,18 +5,18 @@
   #:use-module (guix download)
   #:use-module (guix git-download))
 
-(define-public tdlib-1.8.13
+(define-public tdlib-1.8.14
   (package
    (inherit tdlib)
-   (name "tdlib-1.8.13")
-   (version "1.8.13")
+   (name "tdlib-1.8.14")
+   (version "1.8.14")
    (source (origin
             (method git-fetch)
             (uri (git-reference
                   (url "https://github.com/tdlib/td")
-                  (commit "c95598e5e1493881d31211c1329bdbe4630f6136")))
+                  (commit "66234ae2537a99ec0eaf7b0857245a6e5c2d2bc9")))
             (file-name (git-file-name "tdlib" version))
             (sha256
              (base32
-              "0h435zh4wylzvxd6chzjawa9zibmnnza8nf01zqbaywfg17vpcbp"))))
+              "0lv19vpgv8nqzxmsgnmbg32zjkarqngh8cjhxc3hy3jj74cwkil5"))))
    (arguments '(#:tests? #f))))
